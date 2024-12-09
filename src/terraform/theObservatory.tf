9ceab52d48d4129be09dfc6e9a2aa5ee40083d05
Florian Gruber <csaz8390@student.uibk.ac.at>
2024-12-09 11:08:46 +0100

    removed allow all ingoing traffic on ec2

--- src/terraform/theObservatory.tf
+++ src/terraform/theObservatory.tf
@@ -32,12 +32,6 @@ resource "aws_security_group" "theObservatory_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-      ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
