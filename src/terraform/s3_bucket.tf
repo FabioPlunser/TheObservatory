5e599da6f9892792db8d3e608bb6ab0f83a65585
Florian Gruber <csaz8390@student.uibk.ac.at>
2025-01-02 12:13:01 +0100

    add known faces & faces to check bucket

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,6 +1,9 @@
 # S3 bucket for uploaded images to be checked
 resource "aws_s3_bucket" "the-observatory-faces-to-check" {
   bucket = "the-observatory-faces-to-check"
+    lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_s3_bucket_versioning" "images_versioning" {
@@ -13,6 +16,9 @@ resource "aws_s3_bucket_versioning" "images_versioning" {
 # S3 bucket for known faces
 resource "aws_s3_bucket" "the-observatory-known-faces" {
   bucket = "the-observatory-known-faces"
+    lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_s3_bucket_versioning" "known_faces_versioning" {
@@ -20,4 +26,27 @@ resource "aws_s3_bucket_versioning" "known_faces_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+
+# Upload an known face to the S3 known faces Bucket
+resource "aws_s3_object" "image1" {
+  bucket       = aws_s3_bucket.the-observatory-known-faces.id
+  key          = "PatrickStewart.jpg"    # Key for the object in the bucket
+  source       = "./face_upload/known_faces/PatrickStewart.jpg" # Local path to the image
+  content_type = "image/jpeg"             # MIME type
+}
+# Upload an unknown face to the S3 faces to check Bucket
+resource "aws_s3_object" "image2" {
+  bucket       = aws_s3_bucket.the-observatory-faces-to-check.id
+  key          = "PatrickStewart2004.jpg"    # Key for the object in the bucket
+  source       = "./face_upload/faces_to_check/PatrickStewart2004-08-03.jpg" # Local path to the image
+  content_type = "image/jpeg"             # MIME type
+}
+# Upload an unknown face to the S3 faces to check Bucket
+resource "aws_s3_object" "image3" {
+  bucket       = aws_s3_bucket.the-observatory-faces-to-check.id
+  key          = "pexels-olly.jpg"    # Key for the object in the bucket
+  source       = "./face_upload/faces_to_check/pexels-olly-712513.jpg" # Local path to the image
+  content_type = "image/jpeg"             # MIME type
 }
