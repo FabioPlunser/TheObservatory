9fdeb2ca9b93123fa3afd428c9bf8dd88f548281
Dominik Barbist <d.barbist@hotmail.com>
2025-01-03 15:41:20 +0100

    Update bash script

--- src/run.sh
+++ src/run.sh
@@ -26,7 +26,136 @@ cleanup_spinner() {
     printf "\r%s\n" "$1"
 }
 
+cleanup() {
+    echo "🛑 Stopping all processes..."
+    # Kill any remaining spinner
+    kill $spinner_pid 2>/dev/null || true
+    # Kill camera processes
+    for pid in "${camera_pids[@]}"; do
+        kill $pid 2>/dev/null || true
+    done
+    # Kill alarm processes
+    for pid in "${alarm_pids[@]}"; do
+        kill $pid 2>/dev/null || true
+    done
+    # Kill simulated camera process group
+    if [ "$use_simulated_data" = true ]; then
+        # Kill the entire process group
+        kill -- -$simulated_camera_pid 2>/dev/null || true
+    fi
+    # Kill cloud process
+    if [ "$launch_cloud" = true ]; then
+        kill $cloud_pid 2>/dev/null || true
+    fi
+    # Kill server
+    kill $server_pid 2>/dev/null || true
+    
+    echo "👋 Cleanup complete"
+    rm -rf db.db 
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
 echo "🚀 Starting setup script..."
+
+# Prompt the user to launch or destroy the Terraform server
+read -p "Do you want to launch, destroy, or do nothing with the Terraform server? ([l]aunch/[d]estroy/[n]othing): " action
+if [ "$action" = "l" ]; then
+    # Get user input for launching the Cloud script
+    read -p "Do you want to launch the Cloud script? (y/n): " launch_cloud
+    if [ "$launch_cloud" = "y" ]; then
+        launch_cloud=true
+    else
+        launch_cloud=false
+    fi
+
+    # Check if AWS credentials file exists
+    aws_credentials_path="$HOME/.aws/credentials"
+    if [ ! -f "$aws_credentials_path" ]; then
+        echo "AWS credentials file not found. Prompting for credentials..."
+        read -p "Enter your AWS Access Key: " aws_access_key
+        read -p "Enter your AWS Secret Key: " aws_secret_key
+        read -p "Enter your AWS Region (e.g., us-east-1): " aws_region
+
+        # Configure AWS CLI with the provided credentials
+        aws configure set aws_access_key_id $aws_access_key
+        aws configure set aws_secret_access_key $aws_secret_key
+        aws configure set region $aws_region
+    else
+        echo "AWS credentials file found. Checking for credentials..."
+
+        # Check if the credentials are present
+        if ! grep -q "aws_access_key_id" "$aws_credentials_path" || ! grep -q "aws_secret_access_key" "$aws_credentials_path"; then
+            echo "AWS credentials not found in the file. Prompting for credentials..."
+            read -p "Enter your AWS Access Key: " aws_access_key
+            read -p "Enter your AWS Secret Key: " aws_secret_key
+            read -p "Enter your AWS Region (e.g., us-east-1): " aws_region
+
+            # Configure AWS CLI with the provided credentials
+            aws configure set aws_access_key_id $aws_access_key
+            aws configure set aws_secret_access_key $aws_secret_key
+            aws configure set region $aws_region
+        else
+            echo "AWS credentials found in the file."
+        fi
+    fi
+
+    # Check if the key pair exists
+    key_pair_name="theObservatory"
+    if ! aws ec2 describe-key-pairs --key-names $key_pair_name 2>&1 | grep -q $key_pair_name; then
+        echo "Key pair '$key_pair_name' not found. Creating key pair..."
+        key_pair_path="$HOME/.ssh/theObservatory.pem"
+        aws ec2 create-key-pair --key-name $key_pair_name --query "KeyMaterial" --output text > $key_pair_path
+        chmod 400 $key_pair_path
+        echo "Key pair created and saved to $key_pair_path"
+    else
+        echo "Key pair '$key_pair_name' found."
+        key_pair_path="$HOME/.ssh/theObservatory.pem"
+    fi
+
+    echo "🌍 Initializing Terraform..."
+    pushd terraform
+    terraform init
+
+    echo "🚀 Applying Terraform configuration..."
+    terraform apply -var "private_pem_key=$key_pair_path" -auto-approve
+
+    # Get the IP address of the EC2 instance
+    nats_instance_ip=$(terraform output -raw nats_instance_public_ip)
+    nats_instance_ip=$(echo $nats_instance_ip | xargs) # Trim whitespace
+    
+    popd
+
+    # Start Cloud server
+    if [ "$launch_cloud" = true ]; then
+        echo "🌐 Starting Cloud script..."
+        
+        # Create the full NATS URL
+        natsUrl="nats://$nats_instance_ip:4222"
+
+        # Start the Python process with arguments
+        python cloud/cloud.py $natsUrl &
+        cloud_pid=$!
+    fi
+
+elif [ "$action" = "d" ]; then
+    echo "🛑 Destroying Terraform-managed infrastructure..."
+    pushd terraform
+    terraform destroy -var "private_pem_key=$key_pair_path" -auto-approve
+    popd
+    echo "✅ Terraform-managed infrastructure destroyed."
+    read -p "Do you want to continue with the setup script? (y/n): " close
+    if [ "$close" = "n" ]; then
+        exit
+    fi
+
+elif [ "$action" = "n" ]; then
+    echo "Continue with the rest of the script"
+else
+    echo "🚫 Invalid action. Please enter 'l', 'd', or 'n'."
+    exit 1
+fi
 
 # Create and activate Python virtual environment
 if [ ! -d "venv" ]; then
@@ -39,7 +168,7 @@ echo "🔄 Activating virtual environment..."
 source venv/bin/activate
 
 # Calculate checksum of requirements files
-requirements_files=("server/requirements.txt" "devices/emulated/requirements.txt")
+requirements_files=("onPremise/server/requirements.txt" "onPremise/devices/emulated/requirements.txt" "cloud/requirements.txt")
 checksum=$(cat "${requirements_files[@]}" | sha256sum | awk '{print $1}')
 checksum_file="requirements_checksum.txt"
 
@@ -57,8 +186,9 @@ fi
 # Install Python requirements if needed
 if [ "$run_pip_install" = true ]; then
     echo "📦 Installing required Python packages..."
-    pip install -r server/requirements.txt
-    pip install -r devices/emulated/requirements.txt
+    pip install -r onPremise/server/requirements.txt
+    pip install -r onPremise/devices/emulated/requirements.txt
+    pip install -r cloud/requirements.txt
     echo -n "$checksum" > "$checksum_file"
 else
     echo "📦 Python packages are already up-to-date."
@@ -80,13 +210,13 @@ spinner_pid=$!
 
 # Install frontend dependencies
 if command_exists bun; then
-    (cd server/website && bun install && bun run build) >/dev/null 2>&1 && \
+    (cd onPremise/server/website && bun install && bun run build) >/dev/null 2>&1 && \
     cleanup_spinner "✅ Frontend setup complete!" || {
         cleanup_spinner "❌ Frontend setup failed!"
         exit 1
     }
 elif command_exists npm; then
-    (cd server/website && npm install && npm run build) >/dev/null 2>&1 && \
+    (cd onPremise/server/website && npm install && npm run build) >/dev/null 2>&1 && \
     cleanup_spinner "✅ Frontend setup complete!" || {
         cleanup_spinner "❌ Frontend setup failed!"
         exit 1
@@ -96,22 +226,9 @@ else
     exit 1
 fi
 
-# Start Python dependencies installation
-animate_loading "Installing Python dependencies..." &
-spinner_pid=$!
-
-# Install Python requirements
-{
-    pip install -r server/requirements.txt >/dev/null 2>&1
-    pip install -r devices/emulated/requirements.txt >/dev/null 2>&1
-} && cleanup_spinner "✅ Python dependencies installed!" || {
-    cleanup_spinner "❌ Python dependencies installation failed!"
-    exit 1
-}
-
 # Start the server in the background
 echo "🖥️ Starting edge server..."
-python server/edge_server.py &
+python onPremise/server/edge_server.py &
 server_pid=$!
 
 # Wait for server to start
@@ -123,7 +240,7 @@ echo "📸 Starting $num_cameras camera(s)..."
 camera_pids=()
 for (( i=1; i<=$num_cameras; i++ ))
 do
-    python devices/emulated/camera.py &
+    python onPremise/devices/emulated/camera.py &
     camera_pids+=($!)
 done
 
@@ -131,7 +248,7 @@ done
 if [ "$use_simulated_data" = true ]; then
     echo "📸 Starting simulated cameras ..."
     # Start in a new process group
-    setsid python devices/emulated/simulatedCamera.py &
+    setsid python onPremise/devices/emulated/simulatedCamera.py &
     simulated_camera_pid=$!
 fi
 
@@ -140,37 +257,9 @@ echo "🚨 Starting $num_alarms alarm(s)..."
 alarm_pids=()
 for (( i=1; i<=$num_alarms; i++ ))
 do
-    python devices/emulated/alarm.py &
+    python onPremise/devices/emulated/alarm.py &
     alarm_pids+=($!)
 done
-
-# Trap SIGINT and SIGTERM signals
-cleanup() {
-    echo "🛑 Stopping all processes..."
-    # Kill any remaining spinner
-    kill $spinner_pid 2>/dev/null || true
-    # Kill camera processes
-    for pid in "${camera_pids[@]}"; do
-        kill $pid 2>/dev/null || true
-    done
-    # Kill alarm processes
-    for pid in "${alarm_pids[@]}"; do
-        kill $pid 2>/dev/null || true
-    done
-    # Kill simulated camera process group
-    if [ "$use_simulated_data" = true ]; then
-        # Kill the entire process group
-        kill -- -$simulated_camera_pid 2>/dev/null || true
-    fi
-    # Kill server
-    kill $server_pid 2>/dev/null || true
-    
-    echo "👋 Cleanup complete"
-    rm -rf db.db 
-    exit 0
-}
-
-trap cleanup SIGINT SIGTERM
 
 # Keep script running
 echo "✅ System is running. Press Ctrl+C to stop all processes."
